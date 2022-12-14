fd1873054adce8c67d738970605f27ef2803696f
Vasily <ttp9lhuk93@gmail.com>
2022-12-14 13:57:06 +0300

    lesson 19

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 "use strict";
 
-{
-    let result = 5;
-}
+let numbetOfFilms,
+    lastSeeMovie1,
+    ratingFilm1,
+    lastSeeMovie2,
+    ratingFilm2;
 
-console.log(result);
+numbetOfFilms = prompt('Сколько фильмов смотрел?','');
+
+let personalMovieDB = {
+    count: numbetOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+lastSeeMovie1 = prompt('Один из последних фильмов?','');
+ratingFilm1 = prompt('Насколько оцените его?','');
+lastSeeMovie2 = prompt('Один из последних фильмов?','');
+ratingFilm2 = prompt('Насколько оцените его?','');
+
+personalMovieDB.movies[lastSeeMovie1] = ratingFilm1;
+personalMovieDB.movies[lastSeeMovie2] = ratingFilm2;
+
+console.log(personalMovieDB.movies);
